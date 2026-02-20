5e5e84a6c037107222f3dc47c8f702b630291021
vimalRaj45 <vimalraj5207@gmail.com>
2026-02-21 01:00:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ function generateOTP() {
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_APP_PASSWORD,
+    user: "vimalraj5207@gmail.com",    
+    pass: "txcmjdwgbprjaxrg",            
   },
 });
 
